c1845c7894558e2ba52a6ecec8d4622ef9684c57
Philipp Milovic <milovic.ph@gmail.com>
2022-07-17 01:05:51 +0200

    Fleshed out Sci-Hub request processing

--- internal/fetch/fetch.go
+++ internal/fetch/fetch.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"fmt"
 	"net/http"
+	"net/url"
+	"strings"
 
 	"golang.org/x/net/html"
 )
@@ -23,68 +25,117 @@ type Article struct {
 	Url   string
 }
 
+// Fetch downloads articles from Sci-Hub from a list of supplied DOIs.
 func Fetch(dois []string) error {
 
-	// initialize a list of articles
 	articles := make([]Article, len(dois))
 	for i, d := range dois {
 		articles[i] = Article{Doi: d}
 	}
 
-	// for each article
-	//		send a GET to one of the mirrors
-	//		parse html
-	//			if found(title, url)
-	//				download and write to disc
-	//			else
-	//				return an error
 	for _, a := range articles {
-		// FIXME: should try mirrors in order
-		res, err := http.Get("https://" + mirrors[0] + "/" + a.Doi)
-		if err != nil {
-			return fmt.Errorf("%w", err)
-		}
-		defer res.Body.Close()
-
-		body, err := html.Parse(res.Body)
+		res, err := sendRequest(&a)
 		if err != nil {
 			return fmt.Errorf("%w", err)
 		}
 
-		var getText func(*html.Node, *bytes.Buffer)
-		getText = func(n *html.Node, buf *bytes.Buffer) {
-			if n.Type == html.TextNode {
-				buf.WriteString(n.Data)
-			}
-			for c := n.FirstChild; c != nil; c = c.NextSibling {
-				getText(c, buf)
-			}
+		err = processRequest(res, &a)
+		if err != nil {
+			return fmt.Errorf("%w", err)
 		}
 
-		var f func(*html.Node)
-		f = func(n *html.Node) {
-			if n.Type == html.ElementNode {
-				if n.Data == "button" {
-					for _, a := range n.Attr {
-						if a.Key == "onclick" {
-							fmt.Println(a.Val)
-							break
-						}
-					}
-				}
-				if n.Data == "i" {
-					text := &bytes.Buffer{}
-					getText(n, text)
-					fmt.Println(text)
-				}
-			}
-			for c := n.FirstChild; c != nil; c = c.NextSibling {
-				f(c)
-			}
-		}
-
-		f(body)
+		fmt.Printf("title: %q\n", a.Title)
+		fmt.Printf("link: %q\n", a.Url)
 	}
 
 	return nil
+}
+
+// TODO: reimplement with timeouts and mirror switching
+// sendRequest sends a GET request to 'Sci-Hub/DOI'.
+// The request is sent to a different Sci-Hub mirror if the request times out.
+// An error is returned if a valid response cannot be obtained.
+func sendRequest(a *Article) (*http.Response, error) {
+	res, err := http.Get("https://" + mirrors[0] + "/" + url.QueryEscape(a.Doi))
+	if err != nil {
+		return res, fmt.Errorf("%w", err)
+	}
+
+	return res, nil
+}
+
+// processRequest extracts the article title and URL from a HTML response.
+func processRequest(res *http.Response, a *Article) error {
+	if res.StatusCode > 399 {
+		fmt.Errorf("%s", res.Status)
+	}
+
+	body, err := html.Parse(res.Body)
+	if err != nil {
+		return fmt.Errorf("%w", err)
+	}
+	defer res.Body.Close()
+
+	// NOTE:
+	//	With this approach we're evaluating all nodes each time we want to
+	//	extract a piece of data. Ideally we would want to extract all data
+	//	during the initial (and final) node evaluation.
+	a.Title = getFromHtml(
+		body,
+		func(n *html.Node) bool {
+			return n.Type == html.ElementNode && n.Data == "i"
+		},
+		func(n *html.Node) string {
+			b := &bytes.Buffer{}
+			b.WriteString(n.FirstChild.Data)
+			ss := strings.SplitN(b.String(), ".", -1)
+
+			var s string
+			for i := 0; i < len(ss) - 2; i++ {
+				if len(s) != 0 {
+					s += "."
+				}
+				s += ss[i]
+			}
+			return s
+		},
+	)
+	a.Url = getFromHtml(
+		body,
+		func(n *html.Node) bool {
+			return n.Type == html.ElementNode && n.Data == "button"
+		},
+		func(n *html.Node) string {
+			for _, atr := range n.Attr {
+				if atr.Key == "onclick" {
+					s := strings.TrimPrefix(atr.Val, "location.href='")
+					s = strings.TrimSuffix(s, "?download=true'")
+					return s
+				}
+			}
+			return ""
+		},
+	)
+
+	return nil
+}
+
+// getFromHtmlData walks an HTML tree and extracts data.
+// If the current node in the HTML tree is selected by the 'selector', then
+// data is extracted from the node by the 'exctractor', otherwise another node
+// is selected. Both children and sibling nodes are walked.
+func getFromHtml(
+	n *html.Node,
+	selector func(*html.Node) bool,
+	extractor func(*html.Node) string,
+) string {
+	var s string
+	if selector(n) {
+		s += extractor(n)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		s += getFromHtml(c, selector, extractor)
+	}
+
+	return s
 }
